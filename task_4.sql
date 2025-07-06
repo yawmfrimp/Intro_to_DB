1f8d4d7cee64b192671ebac58c2803c81f6d3a8c
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-06 23:07:09 +0000

    changes to 4

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
 SELECT `COLUMN_NAME`, `COLUMN_TYPE`, `IS_NULLABLE`, `COLUMN_KEY`, `EXTRA`
-FROM `information_schema`.`columns`
-WHERE `table_name` = 'books' AND `table_schema` = 'alx_book_store';
+FROM `INFORMATION_SCHEMA`.`COLUMNS`
+WHERE `TABLE_NAME` = 'Books' AND `TABLE_SCHEMA` = 'alx_book_store';
